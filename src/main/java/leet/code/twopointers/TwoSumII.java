4e00e6d6caf0ee993d91c4b8a7143d73f6a7b2a0
redflesher <redflesher@gmail.com>
2026-02-19 10:55:11 +0100

    commit

--- src/main/java/leet/code/twopointers/TwoSumII.java
+++ src/main/java/leet/code/twopointers/TwoSumII.java
@@ -7,13 +7,12 @@ public class TwoSumII {
         int highIndex = numbers.length - 1;
 
         while (numbers[lowIndex] + numbers[highIndex] != target) {
-            if (numbers[lowIndex] + numbers[highIndex] > target) {
+            if (numbers[lowIndex] + numbers[highIndex] > target)
                 highIndex--;
-            } else // if (numbers[lowIndex] + numbers[highIndex] < target)
-            {
+            else
                 lowIndex++;
-            }
         }
+
         return new int[]{lowIndex + 1, highIndex + 1};
     }
 }
