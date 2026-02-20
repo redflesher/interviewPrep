2687e1d566e7a204289686a03dae8eb91b9941ec
redflesher <redflesher@gmail.com>
2026-02-20 12:18:02 +0100

    commit

---
+++ src/main/java/leet/code/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+package leet.code.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char character = s.charAt(end);
+            if (map.containsKey(character)) {
+                start = Math.max(start, map.get(character) +1);
+            }
+            map.put(character, end);
+            maxLength = Math.max(maxLength, (end - start +1));
+        }
+
+        return maxLength;
+    }
+}
