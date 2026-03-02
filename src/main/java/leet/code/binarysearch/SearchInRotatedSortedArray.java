3556400dd217b5f2401f2305675bce0f64cdba29
redflesher <redflesher@gmail.com>
2026-03-02 10:09:04 +0100

    commit

---
+++ src/main/java/leet/code/binarysearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+package leet.code.binarysearch;
+
+public class SearchInRotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low != high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid])
+                    high = mid;
+                else
+                    low = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+        }
+
+        return nums[low] == target ? low : -1;
+    }
+}
