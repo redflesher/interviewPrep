efcce83bc173d9c6448d6050b1bfe92ff49bac24
redflesher <redflesher@gmail.com>
2026-03-05 12:33:21 +0100

    commit

---
+++ src/main/java/leet/code/binarytree/DFS/ValidateBinarySearchTree.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/validate-binary-search-tree/description/
+package leet.code.binarytree.DFS;
+
+import leet.code.binarytree.TreeNode;
+
+public class ValidateBinarySearchTree {
+    public static boolean isValidBST(TreeNode root) {
+        long min = Long.MIN_VALUE;
+        long max = Long.MAX_VALUE;
+        return preOrderTraversalHelper(root, min, max);
+
+    }
+
+    private static Boolean preOrderTraversalHelper(TreeNode node, long min, long max) {
+        if (node == null)
+           return true;
+
+        if (node.val <= min || node.val >= max)
+            return false;
+
+        return preOrderTraversalHelper(node.left, min, node.val) &&
+                preOrderTraversalHelper(node.right, node.val, max);
+    }
+}
