4735be70c18f0ba853235886c828e6330f7f2468
redflesher <redflesher@gmail.com>
2026-03-06 11:02:15 +0100

    commit

---
+++ src/main/java/leet/code/binarytree/BFS/SerializeAndDeserializeBinaryTree_BFS.java
@@ -0,0 +1,74 @@
+// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+package leet.code.binarytree.BFS;
+
+import leet.code.binarytree.TreeNode;
+
+import java.util.*;
+
+public class SerializeAndDeserializeBinaryTree_BFS {
+
+    // Encodes a tree to a single string.
+    public static String serialize(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root == null)
+            return "";
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+
+            if (currentNode != null) {
+                result.add(String.valueOf(currentNode.val));
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                else
+                    queue.offer(null);
+
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+                else
+                    queue.offer(null);
+            } else
+                result.add("null");
+
+        }
+
+        return String.join(";", result);
+    }
+
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty())
+            return null;
+        String[] nodes = data.split(";");
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode result = new TreeNode(Integer.parseInt(nodes[0]));
+        queue.offer(result);
+        int index = 1;
+
+
+        while(!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+
+            if (!nodes[index].equals("null")) {
+                TreeNode leftNode = new TreeNode(Integer.parseInt(nodes[index]));
+                currentNode.left = leftNode;
+                queue.offer(leftNode);
+            }
+            index++;
+            if (!nodes[index].equals("null")) {
+                TreeNode rightNode = new TreeNode(Integer.parseInt(nodes[index]));
+                currentNode.right = rightNode;
+                queue.offer(rightNode);
+            }
+            index++;
+        }
+        return result;
+    }
+}
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
---
+++ src/main/java/leet/code/binarytree/DFS/SerializeAndDeserializeBinaryTree_DFS.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+package leet.code.binarytree.DFS;
+
+import leet.code.binarytree.TreeNode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SerializeAndDeserializeBinaryTree_DFS {
+    public String serialize (TreeNode root) {
+        if (root == null)
+            return "null";
+
+        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+    }
+
+    public TreeNode deserialize (String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return buildTree(queue);
+    }
+
+    private TreeNode buildTree(Queue<String> queue) {
+        String currentValue = queue.poll();
+        if (currentValue.equals("null"))
+            return null;
+        TreeNode currentNode = new TreeNode(Integer.parseInt(currentValue));
+        currentNode.left = buildTree(queue);
+        currentNode.right = buildTree(queue);
+
+        return currentNode;
+    }
+}
