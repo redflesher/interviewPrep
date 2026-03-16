a203568f7bb90300ba02c5ac9fa41f633bedd069
redflesher <redflesher@gmail.com>
2026-03-16 13:11:15 +0100

    commit

---
+++ src/main/java/leet/code/dynamicprograming/HouseRobber.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/house-robber/description/
+package leet.code.dynamicprograming;
+
+public class HouseRobber {
+    public static int rob(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        if (nums.length == 2)
+            return Math.max(nums[0], nums[1]);
+
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            int current = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
