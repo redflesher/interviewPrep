8c00dc2af44df0deece49cfc5d7193d271a3ac57
redflesher <redflesher@gmail.com>
2026-03-18 10:20:10 +0100

    commit

---
+++ src/main/java/leet/code/dynamicprograming/LongestIncreasingSubsequence.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/description/
+package leet.code.dynamicprograming;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    public static int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int result = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+}
