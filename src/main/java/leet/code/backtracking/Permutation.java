3428397459d9de6c9ff692fe38f8e03924a2cec5
redflesher <redflesher@gmail.com>
2026-04-14 11:30:56 +0200

    commit

---
+++ src/main/java/leet/code/backtracking/Permutation.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/permutations/description/
+package leet.code.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutation {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, result, new ArrayList<>(), used);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> currentPermutation, boolean[] used) {
+        if (currentPermutation.size() == nums.length)
+            result.add(new ArrayList<>(currentPermutation));
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                currentPermutation.add(nums[i]);
+                used[i] = true;
+                backtrack(nums, result, currentPermutation, used);
+                currentPermutation.remove(Integer.valueOf(nums[i]));
+                used[i] = false;
+            }
+        }
+    }
+}
